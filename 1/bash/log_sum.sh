b038066a321039f88a2879ebe6b0480a64f815e7
Eric Nilsson <CaterHatterPillar@gmail.com>
2013-09-09 17:08:57 +0200

    Now supports parsing of mandatory filename. Some refactoring.

--- 1/bash/log_sum.sh
+++ 1/bash/log_sum.sh
@@ -16,53 +16,48 @@
 
 echo "log_sum: Script start."
 
-# Establish bashtrap used to escape application in case of murderous goblins or psychotic mystics:
+# Establish bashtrap used to escape application in case of murderous goblins and/or psychotic mystics:
 trap bashtrap INT
-bashtrap()
-{
-    echo "log_sum: Script force quit. Execution was interrupted."
+bashtrap() {
+	echo "log_sum: Script force quit. Execution was interrupted."
 }
 
 argLimitOutput=-1
 argLimitQueryHours=-1
 argLimitQueryDays=-1
+argFilename=""
 
 # Parse parameters using getopts:
-while getopts ":n:|:h:|:d:|:c|:2|:r|:F|:t|:f" opt; do
-  case $opt in
-    # Limit the number of results to N.
-    n)
-      echo "Limit the number of results to $OPTARG." >&2
-      ;;
-    h)
-      echo "Limit the query to the last number of hours: $OPTARG." >&2
-      ;;
-    d)
-      echo "Limit the query to the last number of days: $OPTARG" >&2
-      ;;
-    c)
-      echo "Which IP address makes the most number of connection attempts?" >&2
-      ;;
-    2)
-      echo "Which address makes the most number of successful attempts?" >&2
-      ;;
-    r)
-      echo "What are the most common results codes and where do they come from?" >&2
-      ;;
-    F)
-      echo "What are the most common result codes that indicate failure (no auth, not found etc) and where do they come from?" >&2
-      ;;
-    t)
-      echo "Which IP number get the most bytes sent to them?" >&2
-      ;;
-    f)
-      echo "Which IP number sends the most bytes to the server <filename> refers to the logfile. If '-' is given as a filename, or no filename is given, then standard input should be read. This enables the script to be used in a pipeline." >&2
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      ;;
-  esac
+while getopts ":n:|:h:d:|:c2rFtf" opt; do
+	case $opt in
+		n)
+			echo "Limit the number of results to $OPTARG." >&2 								;;
+		h)
+			echo "Limit the query to the last number of hours: $OPTARG." >&2 				;;
+		d)
+			echo "Limit the query to the last number of days: $OPTARG" >&2 					;;
+		c)
+			echo "Which IP address makes the most number of connection attempts?" >&2 		;;
+		2)
+			echo "Which address makes the most number of successful attempts?" >&2			;;
+		r)
+			echo "What are the most common results codes and where do they come from?" >&2	;;
+		F)
+			echo "What are the most common result codes that indicate failure (no auth, not found etc) and where do they come from?" >&2 ;;
+		t)
+			echo "Which IP number get the most bytes sent to them?" >&2 ;;
+		f)
+			echo "Which IP number sends the most bytes to the server <filename> refers to the logfile. If '-' is given as a filename, or no filename is given, then standard input should be read. This enables the script to be used in a pipeline." >&2 ;;
+		\?)
+			echo "Invalid option: -$OPTARG." >&2 
+			exit 1 ;;
+	esac
 done
+
+# Parse remaining mandatory parameter (filename):
+argFilename=${@:$OPTIND:1}
+
+#echo "Specified filename: $argFilename"
 
 echo "log_sum: Script end."
 exit 0
