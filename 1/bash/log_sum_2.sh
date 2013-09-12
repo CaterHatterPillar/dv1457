6fd50bad09384ac28e57084d0d216c137f39e477
L0mion <davidgrelsson@gmail.com>
2013-09-12 15:41:45 +0200

    Fixed a bug in log_sum_d.sh where all line where added to first element of array linesSorted

--- 1/bash/log_sum_2.sh
+++ 1/bash/log_sum_2.sh
@@ -1,5 +1,29 @@
 #! /bin/bash
 
+#
+#Layout of log file:
+#ip-adress, ident-answer, username, date and time, request line, http status code, num bytes, referrer page, user agent string
+#
+#
+
+source log_sum_d.sh
+
 function log_sum_2() {
-	echo Function $0 not implemented.
+
+	for d_line in "${linesSorted[@]}"
+	do
+		local ips=$(echo $d_line | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}' )
+		local statusCodes=$(echo $d_line | egrep -o ' [[:digit:]]{3} ' )
+	
+	done
+
+#	index=0
+#	while [ $index -lt ${#ips[@]} ]
+#	do
+#		echo "ip: ${ips[@]}"
+#		echo "StatusCode: ${statusCodes[$index]}"
+#		index=$(expr $index + 1)
+#	done
+
+
 }
