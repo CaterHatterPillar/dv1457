6e557e9f8933c083f29cfc718c4ab1b611e307d3
L0mion <davidgrelsson@gmail.com>
2013-09-12 16:22:30 +0200

    Can find all ip-adresses with success code 200

--- 1/bash/log_sum_2.sh
+++ 1/bash/log_sum_2.sh
@@ -10,20 +10,24 @@ source log_sum_d.sh
 
 function log_sum_2() {
 
-	for d_line in "${linesSorted[@]}"
+	ipIndex=0
+	for line in "${linesSorted[@]}"
 	do
-		local ips=$(echo $d_line | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}' )
-		local statusCodes=$(echo $d_line | egrep -o ' [[:digit:]]{3} ' )
-	
+		ip=$(echo $line | grep ' 200 ' | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}')
+		if [ ${#ip} -gt 1 ]
+		then
+			ips[$ipIndex]=$ip
+			ipIndex=$(expr $ipIndex + 1)
+		fi
 	done
 
-#	index=0
-#	while [ $index -lt ${#ips[@]} ]
-#	do
-#		echo "ip: ${ips[@]}"
-#		echo "StatusCode: ${statusCodes[$index]}"
-#		index=$(expr $index + 1)
-#	done
+	index=0
+	while [ $index -lt ${#ips[@]} ]
+	do
+		echo "ip: ${ips[$index]}"
+		index=$(expr $index + 1)
+ 	done
+
 
 
 }
