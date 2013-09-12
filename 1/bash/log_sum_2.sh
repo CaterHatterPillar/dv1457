0d5e638226deba6ff4d8d487faa46d9da27398c6
L0mion <davidgrelsson@gmail.com>
2013-09-12 17:47:45 +0200

    Can now find all ip-adresses with a successfull conection attempt and print temp in order of have many attempts they have made.

--- 1/bash/log_sum_2.sh
+++ 1/bash/log_sum_2.sh
@@ -10,10 +10,11 @@ source log_sum_d.sh
 
 function log_sum_2() {
 
+	#Find all ip-adresses that have a successfull status code.
 	ipIndex=0
 	for line in "${linesSorted[@]}"
 	do
-		ip=$(echo $line | grep ' 200 ' | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}')
+		ip=$(echo $line | grep ' 200 ' | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3} ')
 		if [ ${#ip} -gt 1 ]
 		then
 			ips[$ipIndex]=$ip
@@ -21,13 +22,22 @@ function log_sum_2() {
 		fi
 	done
 
-	index=0
-	while [ $index -lt ${#ips[@]} ]
+	#Count number of times each ip-adress occurs.
+ 	declare -A ipCount
+	ipMax=0
+	ipWin=0
+	for ip in "${ips[@]}" ; do
+		ipCount[$ip]=$(expr ${ipCount[$ip]} + 1)
+		if (( ${ipCount[$ip]} > ipMax )) ; then
+			ipMax=${ipCount[$ip]}
+			ipWin=$ip
+		fi
+	done
+
+	#Print results and send output thruogh sort to get them in numerical order.
+	for i in "${!ipCount[@]}"
 	do
-		echo "ip: ${ips[$index]}"
-		index=$(expr $index + 1)
- 	done
-
-
-
+		echo "IP: $ip 	Successfull connections: ${ipCount[$i]}"
+	done | sort -k5 -n -r
+	#Sort by fith column in output due to a tab between $ip and Successfull.
 }
