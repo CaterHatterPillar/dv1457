bd7a761be91c8f198e760d6e2cdd9740174d4bc4
Eric Nilsson <CaterHatterPillar@gmail.com>
2013-09-12 15:09:44 +0100

    Implemented query c. Fixed bug causing query c to not recognize all requests from a certain IP.

--- 1/bash/log_sum_c.sh
+++ 1/bash/log_sum_c.sh
@@ -1,25 +1,23 @@
 #! /bin/bash
 
 function log_sum_c() {
+	ips=()
 	for lineSorted in "${linesSorted[@]}"
 	do
-		ip=$(echo $lineSorted | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}' )
-		ips+=$ip
-
-		#echo $ip
+		ip=$(echo $lineSorted | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3} ' )
+		ips[$[${#ips[@]}+1]]=$ip
 	done
 
 	declare -A ipCount
 	ipMax=0
 	ipWin=0
 	for ip in "${ips[@]}" ; do
-		if (( ++ipCount[$ip] > ipMax )) ; then
+		ipCount[$ip]=$(expr ${ipCount[$ip]} + 1)
+		if (( ${ipCount[$ip]} > ipMax )) ; then
 			ipMax=${ipCount[$ip]}
 			ipWin=$ip
 		fi
-
-		#echo $ip
 	done
 
-	#echo "The IP sending most queries is $ipWin with $ipMax requests."
+	echo "The IP sending most queries is $ipWin with $ipMax requests."
 }
