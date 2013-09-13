02013a9f842906539820b7daa65f448033cd884e
L0mion <davidgrelsson@gmail.com>
2013-09-13 11:45:26 +0200

    Can find the ips that recieved the most number of bytes and print them in sccending order

--- 1/bash/log_sum_t.sh
+++ 1/bash/log_sum_t.sh
@@ -1,5 +1,25 @@
 #! /bin/bash
 
+#
+#Which IP gets the most bytes sent to them.
+#
+
 function log_sum_t() {
-	echo Function $0 not implemented.
+
+	index=0
+	for line in "${linesSorted[@]}" ; do
+		numBytes=$(echo $line | egrep -o ' [[:digit:]]{3} [[:digit:]]{1,} ')
+		numBytes=${numBytes:5:${#numBytes}-1}
+	
+		if [ ${#numBytes} -gt 0 ] ; then	
+			ip=$(echo $line | grep $numBytes | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3} ')
+			result="$ip \t $numBytes"
+			results[$index]=$result #"$ip 	$numBytes"
+			index=$(expr $index + 1)
+		fi
+	done
+
+	for result in "${results[@]}" ; do
+		echo -e $result
+	done | sort -k2 -n -r | uniq
 }
